1270b5ada7257ce30a11f6e1814d0a0cdb609f54
Sidharth Juyal <sidharth.juyal@teampicnic.com>
2018-11-25 16:11:53 +0100

    Use shared library in Android client

--- x-platform-djinni/android/app/src/main/java/com/whackylabs/demo/photoapp/MainActivity.java
+++ x-platform-djinni/android/app/src/main/java/com/whackylabs/demo/photoapp/MainActivity.java
@@ -4,11 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.whackylabs.photoapp.Photoapp;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Photoapp photoapp;
 
     // Used to load the 'native-lib' library on application startup.
     static {
-        System.loadLibrary("native-lib");
+        System.loadLibrary("photoapplib");
     }
 
     @Override
@@ -16,14 +20,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        photoapp = Photoapp.create();
+
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        tv.setText(photoapp.getPhotoapp());
     }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 }
