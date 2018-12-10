fb78bdea674499dc70bbab56ce2bbc42c2457d47
Sidharth Juyal <sidharth.juyal@teampicnic.com>
2018-12-10 23:44:03 +0100

    [flutter] Add list

--- flutter/photo_app/lib/main.dart
+++ flutter/photo_app/lib/main.dart
@@ -8,10 +8,7 @@ class PhotoApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: "Photo App",
-      home: Scaffold(
-        appBar: AppBar(title: Text("Photo App")),
-        body: Center(child: RandomWords()),
-      ),
+      home: RandomWords()
     );
   }
 }
@@ -22,9 +19,41 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    final word = WordPair.random();
-    return Text("How you doing? ${word.asPascalCase}");
+    return Scaffold(
+      appBar: AppBar(title: Text("Photo App")),
+      body: _buildSuggestion(),
+    );
+  }
+
+  Widget _buildSuggestion() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) {
+          return Divider();
+        }
+        
+        // [1,2,3,4,5] => [0,1,1,2,2]
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair word) {
+    return ListTile(title: Text(
+      word.asPascalCase,
+      style: _biggerFont,
+      )
+      );
   }
 }
