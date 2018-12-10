ef5aabef5de13625e18b83e2c64a598b79bd2430
Sidharth Juyal <sidharth.juyal@teampicnic.com>
2018-12-10 23:16:05 +0100

    [flutter] Add widget

--- flutter/photo_app/lib/main.dart
+++ flutter/photo_app/lib/main.dart
@@ -4,14 +4,27 @@ import 'package:english_words/english_words.dart';
 void main() => runApp(PhotoApp());
 
 class PhotoApp extends StatelessWidget {
-  @override Widget build(BuildContext context) {
-    final word = WordPair.random();
+  @override 
+  Widget build(BuildContext context) {
     return new MaterialApp(
       title: "Photo App",
       home: Scaffold(
         appBar: AppBar(title: Text("Photo App")),
-        body: Center(child: Text("How you doing? ${word.asPascalCase}")),
+        body: Center(child: RandomWords()),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override 
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final word = WordPair.random();
+    return Text("How you doing? ${word.asPascalCase}");
   }
 }
