7c46bd4c0ef67d844a6e2ae44dd92906f406ced2
Sidharth Juyal <juyal.sidharth@gmail.com>
2024-01-12 19:11:55 +0100

    Init project

---
+++ reactnative/PhotoApp/App.tsx
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default function App(): React.JSX.Element {
+  return (
+    <View style={styles.main}>
+      <Text>Photo App</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
