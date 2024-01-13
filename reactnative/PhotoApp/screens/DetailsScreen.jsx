8ffa0a8ddb667c55eada3b0c0332d9b41dd25acc
Sidharth Juyal <juyal.sidharth@gmail.com>
2024-01-13 17:18:41 +0100

    Add details screen

--- reactnative/PhotoApp/screens/DetailsScreen.jsx
+++ reactnative/PhotoApp/screens/DetailsScreen.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-
-export default function DetailsScreen() {
-  return (
-    <View style={styles.main}>
-      <Text>Details</Text>
-    </View>
-  );
-}
+import {StyleSheet, Text, View, Image} from 'react-native';
 
 const styles = StyleSheet.create({
-  main: {
+  container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 8,
+  },
+  image: {
+    width: 300,
+    height: 300,
+  },
+  title: {
+    fontSize: 18,
+    textAlign: 'center',
+    paddingTop: 8,
   },
 });
+
+export default function DetailsScreen({photo}) {
+  const {title, url} = photo;
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={{uri: url}} />
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+}
