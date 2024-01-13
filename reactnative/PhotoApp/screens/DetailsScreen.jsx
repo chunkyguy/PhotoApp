9625987e248fe85b0b7faa2f2d10b65b10740a34
Sidharth Juyal <juyal.sidharth@gmail.com>
2024-01-13 21:35:25 +0100

    Add networking

--- reactnative/PhotoApp/screens/DetailsScreen.jsx
+++ reactnative/PhotoApp/screens/DetailsScreen.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View, Image} from 'react-native';
+import {StyleSheet, Text, Image} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 /*
--- reactnative/PhotoApp/screens/HomeScreen.jsx
+++ reactnative/PhotoApp/screens/HomeScreen.jsx
@@ -1,55 +1,40 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {ActivityIndicator, SafeAreaView, StyleSheet} from 'react-native';
 import PhotoList from '../components/PhotoList';
 
-const kPhotoList = [
-  {
-    albumId: 1,
-    id: 1,
-    title: 'accusamus beatae ad facilis cum similique qui sunt',
-    url: 'https://via.placeholder.com/600/92c952',
-    thumbnailUrl: 'https://via.placeholder.com/150/92c952',
-  },
-  {
-    albumId: 1,
-    id: 2,
-    title: 'reprehenderit est deserunt velit ipsam',
-    url: 'https://via.placeholder.com/600/771796',
-    thumbnailUrl: 'https://via.placeholder.com/150/771796',
-  },
-  {
-    albumId: 1,
-    id: 3,
-    title: 'officia porro iure quia iusto qui ipsa ut modi',
-    url: 'https://via.placeholder.com/600/24f355',
-    thumbnailUrl: 'https://via.placeholder.com/150/24f355',
-  },
-  {
-    albumId: 1,
-    id: 4,
-    title: 'culpa odio esse rerum omnis laboriosam voluptate repudiandae',
-    url: 'https://via.placeholder.com/600/d32776',
-    thumbnailUrl: 'https://via.placeholder.com/150/d32776',
-  },
-  {
-    albumId: 1,
-    id: 5,
-    title: 'natus nisi omnis corporis facere molestiae rerum in',
-    url: 'https://via.placeholder.com/600/f66b97',
-    thumbnailUrl: 'https://via.placeholder.com/150/f66b97',
-  },
-];
+export default function HomeScreen({navigation}) {
+  const [isLoading, setLoading] = useState(true);
+  const [photoList, setPhotoList] = useState([]);
+
+  const fetchPhotos = async () => {
+    try {
+      setLoading(true);
+      const res = await fetch('https://jsonplaceholder.typicode.com/photos');
+      setPhotoList(await res.json());
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <PhotoList photoList={photoList} navigation={navigation} />
+      )}
+    </SafeAreaView>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
 });
-
-export default function HomeScreen({navigation}) {
-  return (
-    <SafeAreaView style={styles.container}>
-      <PhotoList photoList={kPhotoList} navigation={navigation} />
-    </SafeAreaView>
-  );
-}
