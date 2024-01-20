e1bf4b61ae7c7941de92d066e5cd1bd01271f419
Sidharth Juyal <juyal.sidharth@gmail.com>
2024-01-20 14:23:04 +0100

    Project setup

---
+++ maui/PhotoApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PhotoApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ maui/PhotoApp/Models/Photo.cs
@@ -0,0 +1,10 @@
+﻿namespace PhotoApp;
+
+public class Photo
+{
+    public int albumId { get; set; }
+    public int id { get; set; }
+    public string title { get; set; }
+    public string url { get; set; }
+    public string thumbnailUrl { get; set; }
+}
